dc884dbfc990e7cded69c4055ee139b317c8ab20
novoselovaleksandr <novoselov.aleksandr.96@mail.ru>
2020-08-27 13:48:42 +0500

    auth

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * @format
+ */
+
+import { AppRegistry, YellowBox } from 'react-native'
+import App from './src'
+import { name as appName } from './app.json'
+
+YellowBox.ignoreWarnings([
+  'Warning: AsyncStorage',
+  'Warning: componentWillReceiveProps',
+  'RCTRootView cancelTouches',
+  'not authenticated',
+  'Sending `onAnimatedValueUpdate`'
+])
+
+AppRegistry.registerComponent(appName, () => App)
